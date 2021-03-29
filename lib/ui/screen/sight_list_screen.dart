a06221f8bc800df0fd765141ae6e3a7eb44b885d
Dmitrij Sorokopud <dmitrij.sorokopud@gmail.com>
2021-03-30 01:08:25 +0300

    Создание и стилизация и AppBar экрана SightListScreen

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -9,6 +9,25 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: false,
+        toolbarHeight: 322,
+        title: Padding(
+          padding: const EdgeInsets.only(bottom: 210),
+          child: Text(
+            'Список\nинтересных мест',
+            style: TextStyle(
+                color: const Color(0xff252849),
+                fontFamily: 'Roboto',
+                fontSize: 32,
+                fontWeight: FontWeight.w700),
+          ),
+        ),
+      ),
+      body: SizedBox.shrink(),
+    );
   }
 }
