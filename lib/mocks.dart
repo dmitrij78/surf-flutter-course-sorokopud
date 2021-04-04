5b62e6b5dc15d1cd5ac9b5ade34d441cd401e079
Dmitrij Sorokopud <dmitrij.sorokopud@gmail.com>
2021-04-04 12:00:12 +0300

    Добавлена модель достпримечательностей. Тестовый mock списка достопримечательностей

---
+++ lib/mocks.dart
@@ -0,0 +1,24 @@
+import 'domain/sight.dart';
+
+final mocks = <Sight>[
+  Sight(
+    name: 'Эйфелева башня',
+    details: 'Металлическая башня в центре Парижа, самая'
+        ' узнаваемая его  достопримечательность',
+    lat: 48.51,
+    lon: 2.17,
+    url: 'https://slon.fr/wp-content/uploads/2020/06'
+        '/Eiffel_Tower__night_photo_057981_-1280x853.jpg',
+    type: 'tower',
+  ),
+  Sight(
+    name: 'Статуя сободы',
+    details: 'Колоссальная скульптура в cтиле Наполеона III,'
+        '  или стиле Второй империи, расположенная в США на острове Свободы',
+    lat: 40.41,
+    lon: -74.02,
+    url: 'https://prousa.info/images/cities/statue_of_liberty'
+        '/statue_of_liberty.webp',
+    type: 'monument',
+  ),
+];
