6a346591aa0d4ba8955ddc268d15db1dc56a276b
Dmitrij Sorokopud <dmitrij.sorokopud@gmail.com>
2021-04-11 22:22:21 +0300

    [add] Создан экран VisitingScreen

--- lib/ui/res/strings/strings.dart
+++ lib/ui/res/strings/strings.dart
@@ -1,3 +1,6 @@
 // Экран списка достопримечательностей
 const String appTitle = 'Достопримечательности';
 const String appBarTitleText = 'Список\nинтересных мест';
+
+//  Экран избранное
+const String visitingScreenAppbarTitle = 'Избранное';
---
+++ lib/ui/screen/visiting_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:places/ui/res/strings/strings.dart';
+
+class VisitingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(visitingScreenAppbarTitle),
+      ),
+    );
+  }
+}
